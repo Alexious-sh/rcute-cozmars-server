2fc88f022b5672d53e1bc156278ecdc7e213bcae
Oleksii Shavykin <ganster0x29@gmail.com>
2025-02-23 17:34:44 +0100

    Try #4
    Signed-off-by: Oleksii Shavykin <ganster0x29@gmail.com>

--- rcute_cozmars_server/__main__.py
+++ rcute_cozmars_server/__main__.py
@@ -1,8 +1,5 @@
-from rcute_cozmars_server import create_app
-from sanic.worker.loader import AppLoader
-from functools import partial
+from sanic import Sanic
 
-app_loader = AppLoader(factory=partial(create_app, __name__))
-app = app_loader.load()
+app = Sanic.get_app("rcute_cozmars_server")
 app.run(host="0.0.0.0", port=80, debug=False)
 # app.run(host="0.0.0.0", port=80)
